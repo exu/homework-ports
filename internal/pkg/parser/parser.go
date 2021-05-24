c7cc6c5ff792ffc667e94534c3d170c159dec91a
Jacek Wysocki <jacek.wysocki@kinguin.io>
2021-05-24 08:09:35 +0200

    added JSON streaming parser

---
+++ internal/pkg/parser/parser.go
@@ -0,0 +1,45 @@
+package parser
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+
+	"ports/internal/pkg/pb"
+)
+
+type Callback func(port pb.Port) error
+
+func ProcessPortsJSON(input io.Reader, callback Callback) error {
+	dec := json.NewDecoder(input)
+
+	// read open bracket
+	_, err := dec.Token()
+	if err != nil {
+		return fmt.Errorf("decoding token error: %w", err)
+	}
+
+	// parse map key -> .Token, value -> .Decode
+	for dec.More() {
+		t, err := dec.Token()
+		log.Printf("Scanning %s\n", t)
+		if err != nil {
+			return fmt.Errorf("decode token error: %w", err)
+		}
+		var port pb.Port
+		err = dec.Decode(&port)
+		if err != nil {
+			return fmt.Errorf("decoding error: %w", err)
+		}
+
+		err = callback(port)
+		if err != nil {
+			return fmt.Errorf("calling callback: %w", err)
+		}
+	}
+
+	// read closing bracket
+	_, err = dec.Token()
+	return err
+}
---
+++ internal/pkg/parser/parser_test.go
@@ -0,0 +1,67 @@
+package parser
+
+import (
+	"ports/internal/pkg/model"
+	"ports/internal/pkg/test/assert"
+	"strings"
+	"testing"
+)
+
+const jsonStream = `
+{
+  "AEAJM": {
+    "name": "Ajman",
+    "city": "Ajman",
+    "country": "United Arab Emirates",
+    "alias": [],
+    "regions": [],
+    "coordinates": [
+      55.5136433,
+      25.4052165
+    ],
+    "province": "Ajman",
+    "timezone": "Asia/Dubai",
+    "unlocs": [
+      "AEAJM"
+    ],
+    "code": "52000"
+  },
+  "AEAUH": {
+    "name": "Abu Dhabi",
+    "coordinates": [
+      54.37,
+      24.47
+    ],
+    "city": "Abu Dhabi",
+    "province": "Abu Z¸aby [Abu Dhabi]",
+    "country": "United Arab Emirates",
+    "alias": [],
+    "regions": [],
+    "timezone": "Asia/Dubai",
+    "unlocs": [
+      "AEAUH"
+    ],
+    "code": "52001"
+  }
+
+}
+`
+
+func TestProcessPortsJSON(t *testing.T) {
+
+	t.Run("should process map based json as stream", func(t *testing.T) {
+
+		input := strings.NewReader(jsonStream)
+
+		ports := []model.Port{}
+		ProcessPortsJSON(input, func(port model.Port) error {
+			ports = append(ports, port)
+			return nil
+		})
+
+		assert.Equals(t, "Ajman", ports[0].Name)
+		assert.Equals(t, "Abu Dhabi", ports[1].Name)
+
+	})
+
+}
